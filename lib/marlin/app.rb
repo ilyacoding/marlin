4b149153f3948ec285f75b8d1808269498fe0422
Ilya Kovalenko <ilyacoding@icloud.com>
2019-04-01 13:31:33 +0300

    Fix slave routes

--- lib/marlin/app.rb
+++ lib/marlin/app.rb
@@ -7,23 +7,25 @@ module Marlin
     KEYS_ROUTE = "/keys/:key"
 
     get "/" do
-      "Marlin Key-Value Storage"
+      "Marlin Key-Value Storage (#{ENV['SLAVE_URL'] ? 'Master' : 'Slave' })"
     end
 
     get "/ping" do
       "OK"
     end
 
-    put KEYS_ROUTE do
-      Marlin::Actions::SaveKey.new(params[:key]).call(request.body.read)
+    if ENV['SLAVE_URL']
+      put KEYS_ROUTE do
+        Marlin::Actions::SaveKey.new(params[:key]).call(request.body.read)
+      end
+
+      delete KEYS_ROUTE do
+        Marlin::Actions::DeleteKey.new(params[:key]).call
+      end
     end
 
     get KEYS_ROUTE do
       Marlin::Actions::ReadKey.new(params[:key]).call
-    end
-
-    delete KEYS_ROUTE do
-      Marlin::Actions::DeleteKey.new(params[:key]).call
     end
 
     require File.join(root, "/config/initializers/autoloader.rb")
